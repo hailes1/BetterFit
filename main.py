38e864928d5f435ab319a36a41e1a7b52f3182aa
hailes1 <57600539+hailes1@users.noreply.github.com>
2019-11-10 15:55:55 -0600

    Front end stuff
    All front end part of the application

---
+++ main.py
@@ -0,0 +1,124 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
+from kivy.base import runTouchApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.scrollview import ScrollView
+from kivy.clock import Clock
+from kivy.uix.dropdown import DropDown
+
+
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+class LabelButton(ButtonBehavior, Label):
+    pass
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+
+                self.reset()
+
+                sm.current = "login" 
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    def logOut(self):
+        sm.current = "login"
+    def WorkoutBtn(self):
+        sm.current = "Workoutprogress"
+class WorkoutprogressWindow(Screen):
+    def logOut(self):
+        sm.current = "login"
+class CaloriecountWindow(Screen):
+    def logOut(self):
+        sm.current = "login"
+class GoalWindow(Screen):
+    pass
+class MondayWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
+
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main"),WorkoutprogressWindow(name="Workoutprogress"), CaloriecountWindow(name="Caloriecount"), GoalWindow(name="Goal"), MondayWindow(name="Monday")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class MyMainApp(App):
+    def build(self):
+        return sm
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
